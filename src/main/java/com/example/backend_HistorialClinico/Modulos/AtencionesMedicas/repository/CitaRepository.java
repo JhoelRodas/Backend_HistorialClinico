5017d4caf12f451ec67f2c61a3f3a8e24698c068
Jhoel Rodas <rodasjhoel@gmail.com>
2024-11-12 02:27:29 -0400

    prueba

---
+++ src/main/java/com/example/backend_HistorialClinico/Modulos/AtencionesMedicas/repository/CitaRepository.java
@@ -0,0 +1,13 @@
+package com.example.backend_HistorialClinico.Modulos.AtencionesMedicas.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.backend_HistorialClinico.Modulos.AtencionesMedicas.entity.Cita;
+import com.example.backend_HistorialClinico.Modulos.GestionUsuarios.entity.User;
+
+
+public interface CitaRepository extends JpaRepository<Cita, Integer>{
+    List<Cita> findByUser(User user);
+    List<Cita> findByMedico_User_Id(int userId);
+}
